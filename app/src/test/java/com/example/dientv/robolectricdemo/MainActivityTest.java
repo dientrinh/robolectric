718dda356bfab694245147f77be33c10650a0f0f
dientrinh <dientrinhsy82@gmail.com>
2016-07-27 13:09:15 +0700

    Update MainActivityTest.java

--- app/src/test/java/com/example/dientv/robolectricdemo/MainActivityTest.java
+++ app/src/test/java/com/example/dientv/robolectricdemo/MainActivityTest.java
@@ -14,7 +14,7 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLooper;
-
+import com.example.dientv.robolectricdemo.Utils;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -41,45 +41,51 @@ public class MainActivityTest {
         activity= Robolectric.setupActivity(MainActivity.class);
         assertEquals(4, activity.calculation(2, 2));
     }
-
-    @Test
-    public void test_notNull() {
-        MainActivity mainActivity = buildActivity(MainActivity.class)
-                .create()
-                .start()
-                .resume()
-                .pause()
-                .destroy()
-                .visible()
-                .get();
-        assertThat(mainActivity, not(nullValue()));
-    }
-    @Test
-    public void clickingButton_shouldChangeResultsViewText() throws Exception {
-        activity = Robolectric.setupActivity(MainActivity.class);
-
-        Button btn = (Button) activity.findViewById(R.id.btn);
-        TextView results = (TextView) activity.findViewById(R.id.tv);
-        assertThat(results.getText().toString(), equalTo("123"));
-        btn.performClick();
-        assertThat(results.getText().toString(), equalTo("456"));
-//        Robolectric.flushBackgroundThreadScheduler();
-         ShadowLooper.runUiThreadTasks();
-    }
-    @Test
-    public void testMultiple() throws Exception {
-        activity= Robolectric.setupActivity(MainActivity.class);
-        assertEquals(6, activity.multiple(3, 2));
+    
+      @Test
+    public void cal() throws Exception {
+       
+        assertEquals(4, Utils.cal(2, 2));
     }
 
-    @Test
-    public void test_mock() {
-        SampleA mSampleA = new SampleA();
-        SampleB sampleB = PowerMockito.mock(SampleB.class); // 5
-        PowerMockito.when(sampleB.sampleb()).thenReturn("B");
-        Whitebox.setInternalState(mSampleA, "mSampleB", sampleB);
-       // Assert.assertEquals("B", mSampleA.getStr());
-        assertThat("B", equalTo(mSampleA.getStr()));
-        assertThat("B", equalTo(sampleB.sampleb()));
-    }
+//     @Test
+//     public void test_notNull() {
+//         MainActivity mainActivity = buildActivity(MainActivity.class)
+//                 .create()
+//                 .start()
+//                 .resume()
+//                 .pause()
+//                 .destroy()
+//                 .visible()
+//                 .get();
+//         assertThat(mainActivity, not(nullValue()));
+//     }
+//     @Test
+//     public void clickingButton_shouldChangeResultsViewText() throws Exception {
+//         activity = Robolectric.setupActivity(MainActivity.class);
+
+//         Button btn = (Button) activity.findViewById(R.id.btn);
+//         TextView results = (TextView) activity.findViewById(R.id.tv);
+//         assertThat(results.getText().toString(), equalTo("123"));
+//         btn.performClick();
+//         assertThat(results.getText().toString(), equalTo("456"));
+// //        Robolectric.flushBackgroundThreadScheduler();
+//          ShadowLooper.runUiThreadTasks();
+//     }
+//     @Test
+//     public void testMultiple() throws Exception {
+//         activity= Robolectric.setupActivity(MainActivity.class);
+//         assertEquals(6, activity.multiple(3, 2));
+//     }
+
+//     @Test
+//     public void test_mock() {
+//         SampleA mSampleA = new SampleA();
+//         SampleB sampleB = PowerMockito.mock(SampleB.class); // 5
+//         PowerMockito.when(sampleB.sampleb()).thenReturn("B");
+//         Whitebox.setInternalState(mSampleA, "mSampleB", sampleB);
+//       // Assert.assertEquals("B", mSampleA.getStr());
+//         assertThat("B", equalTo(mSampleA.getStr()));
+//         assertThat("B", equalTo(sampleB.sampleb()));
+//     }
 }
